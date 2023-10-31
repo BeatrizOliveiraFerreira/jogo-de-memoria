e298c10cac2bf747a672b96c9dc5528918073c1a
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-10-31 11:35:30 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,70 @@
+const emojis = [
+    "😎",
+    "😎",
+    "😊",
+    "😊",
+    "😴",
+    "😴",
+    "🤑",
+    "🤑",
+    "😱",
+    "😱",
+    "👽",
+    "👽",
+    "🦊",
+    "🦊",
+    "🥰",
+    "🥰",
+];
+let openCards = [];
+
+let shuffeEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffeEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+
+}
+
+function handleClick(){
+    if (openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+
+    playSound("hit");
+};
+
+
+function checkMatch(){
+
+
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList .remove("boxOpen");
+        openCards[1].classList .remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length===emojis.length){
+        alert("Voce ganhou!!!!")
+    }
+
+}
+
+  function playSound(audioName) {
+    const audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+  }
